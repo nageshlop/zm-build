05bd375efd1ba68a08bdee3e8f552918e8b728e0
Umashankar Avagadda <umagmrit@gmail.com>
2021-01-07 07:25:31 -0500

    Changes for 9.0.0 FOSS

--- rpmconf/Install/Util/globals.sh
+++ rpmconf/Install/Util/globals.sh
@@ -47,6 +47,11 @@ zimbra-license-extension \
 zimbra-network-store \
 zimbra-network-modules-ng"
 
+MYDIR="$(CDPATH= cd "$(dirname "$0")" && pwd)"
+if [ "$(cat ${MYDIR}/.BUILD_TYPE)" == "NETWORK" ]; then
+    OPTIONAL_PACKAGES="${OPTIONAL_PACKAGES} zimbra-modern-ui zimbra-modern-zimlets"
+fi
+
 CHAT_PACKAGES="zimbra-chat \
 zimbra-connect \
 zimbra-talk"
